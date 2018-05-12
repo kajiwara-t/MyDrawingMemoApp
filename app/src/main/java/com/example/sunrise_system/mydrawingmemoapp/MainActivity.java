f85ea4255b6e78ce3c55ffd4b156f36c4f3d54b9
t-kajiwara <kajiwara@sunrise-system.com>
2018-05-12 15:31:38 +0900

    手書きメモアプリ(2018/5/12)

---
+++ app/src/main/java/com/example/sunrise_system/mydrawingmemoapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sunrise_system.mydrawingmemoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.graphics.Color;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //coordinates=座標
+
+}
